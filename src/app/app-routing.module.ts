3011b1786feb5a228bc94033624ed432d8d4a19a
Raghu Khanal <rkhanal@xpanxion.com>
2021-04-19 16:33:31 -0400

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerOrderComponent } from './components/customer-order/customer-order.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component:HomeComponent},
+  { path:'customer', component:CustomerComponent},
+  { path:'employee', component:EmployeeComponent},
+  { path:'product', component:ProductComponent},
+  { path:'customerorder', component:CustomerOrderComponent},
+  { path:'nav', component:NavComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { ProductComponent } from './components/product/product.component';
+import { CustomerOrderComponent } from './components/customer-order/customer-order.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    EmployeeComponent,
+    ProductComponent,
+    CustomerOrderComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
