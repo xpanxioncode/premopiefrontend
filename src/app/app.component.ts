39c674226a99e3d546e49b226b5acdaa23c3fd2f
Raghu Khanal <rkhanal@xpanxion.com>
2021-04-26 12:32:50 -0400

    success message done, working on order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var calculatePrice: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'premopieAngular';
 }
+
+
+
+
